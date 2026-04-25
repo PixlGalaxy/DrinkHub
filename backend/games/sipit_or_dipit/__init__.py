d48839aece75f28a3b906595d2aac4ee26c19c95
PixlGalaxy <fabrizio.gamboa.p@gmail.com>
2026-04-24 23:26:13 -0400

    chore: create init py files for package structure

---
+++ backend/games/sipit_or_dipit/__init__.py
@@ -0,0 +1,13 @@
+from .cards import ALL_CARDS, CHALLENGE_CARDS, TRUTH_CARDS, RULE_CARDS, PENALTY_CARDS
+from .service import build_deck, GAME_ID, GAME_NAME
+
+__all__ = [
+    "ALL_CARDS",
+    "CHALLENGE_CARDS",
+    "TRUTH_CARDS",
+    "RULE_CARDS",
+    "PENALTY_CARDS",
+    "build_deck",
+    "GAME_ID",
+    "GAME_NAME",
+]
